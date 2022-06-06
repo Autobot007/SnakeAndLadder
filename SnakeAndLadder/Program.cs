5aa0504c7a0b520950458b3cf1664c8dfad1e24a
Autobot007 <s0rf0r00z50@gmail.com>
2022-06-06 18:03:54 +0530

    UC3 COMPLETED

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,9 +1,60 @@
 ﻿namespace SnakeLadder;
-public class SnakeLadderuc1
+public class SnakeLadderuc3
 {
+   public int PLAYER1_POS = 10;
+    public int DICE;
+    public int DICE_ROLLED = 0;
+    public int PLAYCHECK=0;
     public static void Main(string[] args)
     {
-        int player1pos = 0;
-        Console.WriteLine("Player 1 is At " + player1pos);
+        SnakeLadderuc3 obj = new();
+        obj.Dice();
+        obj.playcheck();
+
+        
     }
+    public void playcheck()
+    {
+        Random playcheck = new();
+        int PLAYCHECK = Convert.ToInt32(playcheck.Next(0, 3));
+        switch (PLAYCHECK)
+        {
+            case 0:
+                //No Play
+                PLAYER1_POS++;
+                DICE_ROLLED++;
+                Console.WriteLine("DICE ROLLED"+DICE_ROLLED);
+                Console.WriteLine("DICE " + DICE);
+                Console.WriteLine("NO PLAY");
+                Console.WriteLine("NO PLAY" + PLAYER1_POS);
+
+                break;
+            case 1:
+                //Ladder
+                PLAYER1_POS += DICE;
+                DICE_ROLLED++;
+                Console.WriteLine("DICE ROLLED" + DICE_ROLLED);
+                Console.WriteLine("DICE " + DICE);
+                Console.WriteLine("LADDER");
+                Console.WriteLine("LADDER"+PLAYER1_POS);
+                break;
+            case 2:
+                //snake
+                PLAYER1_POS -= DICE;
+                DICE_ROLLED++;
+                Console.WriteLine("DICE ROLLED" + DICE_ROLLED);
+                Console.WriteLine("DICE " + DICE);
+                Console.WriteLine("SNAKE");
+                Console.WriteLine("SNAKE" + PLAYER1_POS);
+                break;
+
+        }
+    }
+    public void Dice()
+    {
+        Random dice = new();
+        DICE = dice.Next(1, 7);
+
+    }
+    
 }
