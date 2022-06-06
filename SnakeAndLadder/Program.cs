16bec7baf9a4204a0c8439aba8aaf0529469b0e6
Autobot007 <s0rf0r00z50@gmail.com>
2022-06-06 15:17:00 +0530

    UC2 Completed

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace SnakeLadder;
-public class SnakeLadderuc1
+public class SnakeLadderuc2
 {
     public static void Main(string[] args)
     {
         int player1pos = 0;
         Console.WriteLine("Player 1 is At " + player1pos);
+        RollDice dice = new();
+        dice.rolldice();
+
     }
 }
---
+++ SnakeAndLadder/RollDice.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeLadder
+{
+    internal class RollDice
+    {
+        Random diceroll = new();
+        public int rolldice()
+        {
+           int dice= diceroll.Next(1, 7);
+            Console.WriteLine(dice);
+            return dice;
+        }
+    }
+}
