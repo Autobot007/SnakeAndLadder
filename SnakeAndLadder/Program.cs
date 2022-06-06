c0621c2e21dbe2d280da1d141cf289ff118c2f7e
Autobot007 <s0rf0r00z50@gmail.com>
2022-06-06 14:13:55 +0530

    UC1 Completed

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SnakeLadder;
+public class SnakeLadderuc1
+{
+    public static void Main(string[] args)
+    {
+        int player1pos = 0;
+        Console.WriteLine("Player 1 is At " + player1pos);
+    }
+}
